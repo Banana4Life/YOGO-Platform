9f9ff1fe808f95c90d50b34f9fb6c3e476bc8867
Phillip Schichtel <phillip.public@schich.tel>
2013-12-16 11:55:08 +0100

    pause when losing focus and some refactoring in PlatformGame

--- src/de/aima13/platform/PlatformGame.java
+++ src/de/aima13/platform/PlatformGame.java
@@ -1,7 +1,6 @@
 package de.aima13.platform;
 
 import java.awt.Font;
-import java.awt.event.KeyEvent;
 import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,7 +11,6 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
-import org.newdawn.slick.KeyListener;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.StateBasedGame;
@@ -29,30 +27,27 @@ import de.aima13.platform.states.Pause;
 import de.aima13.platform.util.CheatEngine;
 
 public class PlatformGame extends StateBasedGame {
-    
+
     public CheatEngine       cheatEngine;
-    
-    private AppGameContainer app;
+
     /** The fonts to draw to the screen */
     public TrueTypeFont      fontDefault;
     public TrueTypeFont      fontHeader;
-    
-    private GameContainer    container;
+
     private GameLevel        level;
-    
+
     public Image             imageColorA, imageColorB;
     public Animation         shaderColor;
     public boolean           shaderColorActive;
-    
+
     public Color             globalTextColor = Color.white;
-    
+
     public PlatformGame() {
         super("#YOGO Platform");
-        
     }
-    
+
+    @Override
     public void initStatesList(GameContainer container) throws SlickException {
-        this.container = container;
         level = new GameLevel(this, container.getInput());
         level.init();
         addState(new MainMenu());
@@ -62,38 +57,37 @@ public class PlatformGame extends StateBasedGame {
         addState(new Loose());
         init();
     }
-    
+
     public void init() throws SlickException {
         fontDefault = loadFont("res/font/minecraft.ttf");
         fontHeader = loadFont("res/font/minecraft.ttf", 54f);
-        
         imageColorA = new Image("res/images/shader/color-a.png");
         imageColorB = new Image("res/images/shader/color-b.png");
         shaderColor = new Animation(new Image[] { imageColorA, imageColorB }, 25);
         cheatEngine = new CheatEngine(this);
         shaderColorActive = false;
     }
-    
+
     public TrueTypeFont loadFont(String res) {
         return loadFont(res, Font.PLAIN);
     }
-    
+
     public TrueTypeFont loadFont(String res, float size) {
         return loadFont(res, size, Font.PLAIN);
     }
-    
+
     public TrueTypeFont loadFont(String res, int style) {
         return loadFont(res, 18f, style);
     }
-    
+
     public TrueTypeFont loadFont(String res, float size, int style) {
         return loadFont(res, size, style, true);
     }
-    
+
     public TrueTypeFont loadFont(String res, float size, int style, boolean antiAlias) {
         try {
             InputStream inputStream = ResourceLoader.getResourceAsStream(res);
-            
+
             Font ttf = Font.createFont(Font.TRUETYPE_FONT, inputStream);
             ttf = ttf.deriveFont(style, size);
             return new TrueTypeFont(ttf, antiAlias);
@@ -102,25 +96,12 @@ public class PlatformGame extends StateBasedGame {
         }
         return null;
     }
-    
-    public void keyPressed(int key, char c) {
-        super.keyPressed(key, c);
-        if (key == Input.KEY_ESCAPE) {
-            // game.getContainer().exit();
-        }
-        if (key == Input.KEY_F1) {
-            if (app != null) {
-                try {
-                    int fullhd = 1;
-                    app.setDisplayMode(fullhd * 1920, fullhd * 1080, false);
-                    app.reinit();
-                } catch (Exception e) {
-                    Log.error(e);
-                }
-            }
-        }
-    }
-    
+
+//    @Override
+//    public void keyPressed(int key, char c) {
+//        super.keyPressed(key, c);
+//    }
+
     @Override
     public void keyReleased(int key, char c) {
         super.keyReleased(key, c);
@@ -128,34 +109,36 @@ public class PlatformGame extends StateBasedGame {
             cheatEngine.onKeyPress(key, c);
         }
     }
-    
-    public AppGameContainer getApp() {
-        return app;
-    }
-    
-    public static void main(String[] args) {
-        try {
-            AppGameContainer appgc;
-            appgc = new AppGameContainer(new PlatformGame());
-            appgc.setDisplayMode(640, 480, false);
-            appgc.setTargetFrameRate(60);
-            appgc.setShowFPS(false);
-            appgc.start();
-        } catch (SlickException ex) {
-            Logger.getLogger(PlatformGame.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
+
     public GameLevel getLevel() {
         return level;
     }
-    
+
     public void lose() {
         enterState(Loose.ID, new EmptyTransition(), new FadeInTransition(Color.black));
         try {
             ((Game) getState(Game.ID)).resetState();
         } catch (SlickException e) {
             e.printStackTrace(System.err);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
+            AppGameContainer appgc;
+            appgc = new AppGameContainer(new PlatformGame());
+            appgc.setDisplayMode(Math.round(640 * appgc.getAspectRatio()), Math.round(appgc.getScreenHeight() - (100 * appgc.getAspectRatio())), false);
+            appgc.setUpdateOnlyWhenVisible(false);
+            appgc.setTargetFrameRate(60);
+            appgc.setShowFPS(false);
+            appgc.start();
+        }
+        catch (SlickException ex)
+        {
+            Logger.getLogger(PlatformGame.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
--- src/de/aima13/platform/states/Game.java
+++ src/de/aima13/platform/states/Game.java
@@ -25,14 +25,14 @@ import de.aima13.platform.entity.Platform;
 import de.aima13.platform.util.Vector;
 
 public class Game extends BasicGameState {
-    
+
     public final static int ID = 1;
     private PlatformGame    game;     // stored for later use
     private GameContainer   container;
     private GameLevel       level;
-    
+
     private Platform        platform;
-    
+
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
         if (game instanceof PlatformGame) {
             this.game = (PlatformGame) game;
@@ -42,21 +42,21 @@ public class Game extends BasicGameState {
         this.container = container;
         resetState();
     }
-    
+
     public void resetState() throws SlickException {
         this.level = game.getLevel();
         level.reset();
-        
+
         PowerBar powerBar = level.spawn(new PowerBar());
         CooldownBar cooldownBar = level.spawn(new CooldownBar());
         Points points = level.spawn(new Points());
         platform = level.spawn(new Platform(powerBar, cooldownBar, 10));
-        
+
         Creature creature = level.spawn(new Creature(platform, points));
-        
+
         BatterySpawner spawner = level.spawn(new BatterySpawner());
     }
-    
+
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
         this.level.render(g);
@@ -64,17 +64,21 @@ public class Game extends BasicGameState {
             this.game.shaderColor.draw(0, 0, this.game.getContainer().getWidth(), this.game.getContainer().getHeight());
         }
     }
-    
+
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+        if (!this.game.getContainer().hasFocus())
+        {
+            this.game.enterState(Pause.ID, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
+        }
         this.level.update(delta);
     }
-    
+
     @Override
     public int getID() {
         return ID;
     }
-    
+
     public void keyReleased(int key, char c) {
         switch (key) {
             case Input.KEY_ESCAPE:
@@ -84,5 +88,5 @@ public class Game extends BasicGameState {
                 break;
         }
     }
-    
+
 }
