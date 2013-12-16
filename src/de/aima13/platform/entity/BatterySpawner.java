3ba9aaf35fa3cf71bcbffe22cf12bbcd30c00bc6
Phillip Schichtel <phillip.public@schich.tel>
2013-12-16 12:01:38 +0100

    jump harder!

--- src/de/aima13/platform/entity/BatterySpawner.java
+++ src/de/aima13/platform/entity/BatterySpawner.java
@@ -9,11 +9,11 @@ import org.newdawn.slick.Graphics;
 import java.util.Random;
 
 public class BatterySpawner extends Entity {
-    private static final long DELAY       = 1000 * 1;
-    
+    private static final long DELAY       = (long)(1000 * .1);
+
     private long              lastSpawned = 0;
     private Random            random      = new Random();
-    
+
     @Override
     public void onInit() {
         move(1, -10);
@@ -21,7 +21,7 @@ public class BatterySpawner extends Entity {
         this.setBB(new Box(40, 1));
         this.setVelocity(new Vector(3, 0));
     }
-    
+
     @Override
     public void update(int delta) {
         long current = System.currentTimeMillis();
@@ -30,13 +30,13 @@ public class BatterySpawner extends Entity {
             getLevel().spawn(new Battery(random.nextFloat() * .3f - .1f)).move(getPosition().add(getBB().getWidth() / 2, 0));
         }
     }
-    
+
     @Override
     public void render(Graphics g) {
         g.setColor(Color.yellow);
         g.fillRect(getPosition().x - 10, getPosition().y - 10, 20, 20);
     }
-    
+
     @Override
     public void onCollideWithBorder(Face collidedFace) {
         switch (collidedFace) {
