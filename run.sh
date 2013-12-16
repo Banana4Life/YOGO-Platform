e54cdaa5a37514cf8fba2f18daac598614d54d2a
Phillip Schichtel <quick_wango@code-infection.de>
2013-12-16 23:55:55 +0100

    run.sh should work now

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 script_path="$(dirname "$(readlink -f "$0")")"
-javaw "-Djava.library.path=$script_path/natives" -jar "$script_path/#yogo-platform.jar"
+java "-Djava.library.path=$script_path/natives" -jar "$script_path/yogo-platform.jar" &
